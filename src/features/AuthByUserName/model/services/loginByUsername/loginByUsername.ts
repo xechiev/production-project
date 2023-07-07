3d64bac978575875fefbabeadbfe0a2d13599186
Mingiian <khechievm08@yandex.ru>
2023-07-07 18:02:28 -0400

    add login module

--- src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.ts
@@ -2,7 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { userActions } from 'entities/User';
 import { User } from 'entities/User/model/types/user';
-import i18n from 'shared/config/i18n/i18n';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 interface LoginByUsernameProps {
@@ -30,7 +29,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps>(
         } catch (error) {
             // eslint-disable-next-line no-console
             console.log(error);
-            return thunkAPI.rejectWithValue(i18n.t('Неверный логин или пароль'));
+            return thunkAPI.rejectWithValue('Error');
         }
     },
 );
--- src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
@@ -43,7 +43,12 @@ export const LoginForm = memo((props: LoginFormProps) => {
     return (
         <div className={classNames(cls.loginForm, {}, [className])}>
             <Text title={t('Форма авторизации')} />
-            {error && <Text text={error} theme={TextTheme.ERROR} />}
+            {error && (
+                <Text
+                    text={t('Вы ввели неверный логин или пароль')}
+                    theme={TextTheme.ERROR}
+                />
+            )}
             <Input
                 className={cls.input}
                 placeholder={t('логин')}
