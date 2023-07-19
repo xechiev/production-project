a8fd9f49205f4b58e424b5aae3532ff166ed2467
Mingiian <khechievm08@yandex.ru>
2023-07-19 16:53:13 -0400

    fix lint

--- src/shared/lib/tests/TextAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TextAsyncThunk/TestAsyncThunk.ts
@@ -1,5 +1,6 @@
 import { AsyncThunkAction, Dispatch } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
+import axios, { AxiosStatic } from 'axios';
 
 type ActionCreatorType<Return, PropsArgs, RejectedValue> = (
     // eslint-disable-next-line no-unused-vars
@@ -12,6 +13,10 @@ type ActionCreatorType<Return, PropsArgs, RejectedValue> = (
     }
 >;
 
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
+
 export class TestAsyncThunk<Return, PropsArgs, RejectedValue> {
     dispatch: Dispatch;
 
@@ -19,17 +24,27 @@ export class TestAsyncThunk<Return, PropsArgs, RejectedValue> {
 
     actionCreator: ActionCreatorType<Return, PropsArgs, RejectedValue>;
 
+    api: jest.MockedFunctionDeep<AxiosStatic>;
+
+    navigate: jest.MockedFn<any>;
+
     constructor(
         actionCreator: ActionCreatorType<Return, PropsArgs, RejectedValue>,
     ) {
         this.actionCreator = actionCreator;
         this.dispatch = jest.fn();
         this.getState = jest.fn();
+
+        this.api = mockedAxios;
+        this.navigate = jest.fn();
     }
 
     async callThunk(arg: PropsArgs) {
         const action = this.actionCreator(arg);
-        const result = await action(this.dispatch, this.getState, undefined);
+        const result = await action(this.dispatch, this.getState, {
+            api: this.api,
+            navigate: this.navigate,
+        });
 
         return result;
     }
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -10,19 +10,20 @@ export interface componentRenderOptions {
     initialState?: DeepPartial<StateSchema>;
 }
 
-export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
-    const {
-        route = '/',
-        initialState,
-    } = options;
+export function componentRender(
+    component: ReactNode,
+    options: componentRenderOptions = {},
+) {
+    const { route = '/', initialState } = options;
 
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            {' '}
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>,
     );
 }
